c0a6cbd56f0f7d61f6112078c9dd90a9cc25cb82
Ayoub Saif <ayoubsaif@gmail.com>
2023-05-01 01:43:51 +0200

    add new routes and methods for courses

--- backend/app/routes/routes.php
+++ backend/app/routes/routes.php
@@ -1,6 +1,8 @@
 <?php
 
+require_once 'app/middleware/Middleware.php';
 require_once 'app/controllers/UserController.php';
+require_once 'app/controllers/CoursesController.php';
 
 $request_uri = explode('?', $_SERVER['REQUEST_URI'], 2);
 
@@ -13,6 +15,36 @@ switch ($request_uri[0]) {
     case '/api/login':
         $controller = new UserController();
         $controller->login();
+        break;
+
+    case '/api/courses':
+        $controller = new CoursesController();
+        $controller->getAll();
+        break;
+
+    case '/api/course':
+        switch ($_SERVER['REQUEST_METHOD']){
+            case 'GET':
+                $controller = new CoursesController();
+                $controller->getOne();
+                break;
+            case 'POST':
+                $controller = new CoursesController();
+                $controller->create();
+                break;
+            case 'PUT':
+                $controller = new CoursesController();
+                //$controller->update();
+                break;
+            case 'DELETE':
+                $controller = new CoursesController();
+                //$controller->delete();
+                break;
+            default:
+                http_response_code(404);
+                echo json_encode(array("message" => "Route not found"));
+                break;
+        }
         break;
 
     default:
