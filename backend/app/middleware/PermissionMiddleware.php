44f9769d83df38866cbc2d4926438dc65bb408f2
Ayoub Saif <ayoubsaif@gmail.com>
2023-05-01 10:37:14 +0200

    add error if not authorization sent in request

--- backend/app/middleware/PermissionMiddleware.php
+++ backend/app/middleware/PermissionMiddleware.php
@@ -12,33 +12,37 @@ class PermissionMiddleware extends Middleware {
         $headers = apache_request_headers();
 
         if (isset($headers['Authorization'])) {
-            $authorizationHeader = $headers['Authorization'];
+                $authorizationHeader = $headers['Authorization'];
 
-            // Comprobamos si el encabezado contiene la etiqueta 'Bearer'
-            if (preg_match('/Bearer\s(\S+)/', $authorizationHeader, $matches)) {
-                $token = $matches[1];
-                if ($token) {
-                    $user = new UserController();
-                    $user = $user->validateToken($token);
-                    if ($user) {
-                        if (in_array($user->permissionType, $allowed)) {
-                            return $user;
+                // Comprobamos si el encabezado contiene la etiqueta 'Bearer'
+                if (preg_match('/Bearer\s(\S+)/', $authorizationHeader, $matches)) {
+                    $token = $matches[1];
+                    if ($token) {
+                        $user = new UserController();
+                        $user = $user->validateToken($token);
+                        if ($user) {
+                            if (in_array($user->permissionType, $allowed)) {
+                                return $user;
+                            } else {
+                                http_response_code(403);
+                                echo json_encode(array("message" => "You don't have permission to access this resource"));
+                                return false;
+                            }
                         } else {
-                            http_response_code(403);
-                            echo json_encode(array("message" => "You don't have permission to access this resource"));
+                            http_response_code(401);
+                            echo json_encode(array("message" => "Invalid token"));
                             return false;
                         }
-                    } else {
-                        http_response_code(401);
-                        echo json_encode(array("message" => "Invalid token"));
-                        return false;
                     }
-                }
+            } else {
+                http_response_code(401);
+                echo json_encode(array("message" => "No token provided"));
+                return false;
+            }
         } else {
             http_response_code(401);
-            echo json_encode(array("message" => "No token provided"));
+            echo json_encode(array("message" => "No Authorization provided"));
             return false;
         }
-    }
 }
 }
