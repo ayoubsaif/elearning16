b7658d8a87c5d755add903e46fb530a35fa9258e
Ivan Goncalves <ivangoncalves727@gmail.com>
2023-05-07 18:54:19 +0200

    cambios CategoriesController

--- backend/app/controllers/CategoriesController.php
+++ backend/app/controllers/CategoriesController.php
@@ -36,6 +36,60 @@ class CategoriesController
         }
     }
 
+    public function update($id)
+    {
+        $data = json_decode(file_get_contents("php://input"));
+        
+        $PermissionMiddleware = new PermissionMiddleware();
+        $allowed = array('admin','teacher');
+        $UserPermmited = $PermissionMiddleware->handle($allowed);
+        if (!$UserPermmited) {
+            return;
+        }
+
+        $category = new CategoryModel();
+        $category->id = $id;
+        $category->name = $data->name;
+        $category->slug = $data->slug;
+        $category->description = $data->description;
+        $category->image_url = $data->image_url;
+        $category->parent_cat = $data->parent_cat;
+        $category->keywords = implode(",", $data->keywords);
+        $category->create_uid = $UserPermmited->id;
+
+        if ($category->update()) {
+            http_response_code(200);
+            echo json_encode(array("message" => "Category was updated"));
+            return;
+        } else {
+            http_response_code(503);
+            echo json_encode(array("message" => "Unable to update category"));
+            return;
+        }
+    }
+
+    public function delete($id)
+    {
+        $PermissionMiddleware = new PermissionMiddleware();
+        $allowed = array('admin','teacher');
+        $UserPermmited = $PermissionMiddleware->handle($allowed);
+        if (!$UserPermmited) {
+            return;
+        }
+
+        $category = new CategoryModel();
+        $category->id = $id;
+
+        if ($category->delete($id)) {
+            http_response_code(200);
+            echo json_encode(array("message" => "Category was deleted"));
+            return;
+        } else {
+            http_response_code(503);
+            echo json_encode(array("message" => "Unable to delete category"));
+            return;
+        }
+    }
 
     public function getAll()
     {
@@ -66,6 +120,15 @@ class CategoriesController
 
     public function getOne($slug)
     {
+        $data = json_decode(file_get_contents("php://input"));
+
+        $PermissionMiddleware = new PermissionMiddleware();
+        $allowed = array('admin','teacher');
+        $UserPermmited = $PermissionMiddleware->handle($allowed);
+        if (!$UserPermmited) {
+            return;
+        }
+
         try {
             $category = new CategoryModel();
             $slugArray = explode("-", $slug);
