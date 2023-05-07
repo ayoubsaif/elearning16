304564113351175e842fee5c8c738b964324fa97
Ayoub Saif <ayoubsaif@gmail.com>
2023-05-07 19:52:05 +0200

    update database structure fixing foreign key of attachments with course_content

--- database.sql
+++ database.sql
@@ -1,154 +1,292 @@
-/*
- Navicat Premium Data Transfer
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 07-05-2023 a las 19:50:33
+-- Versión del servidor: 10.4.22-MariaDB
+-- Versión de PHP: 8.1.0
 
- Source Server         : 127.0.0.1
- Source Server Type    : MySQL
- Source Server Version : 100422
- Source Host           : localhost:3306
- Source Schema         : elearning_16
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
- Target Server Type    : MySQL
- Target Server Version : 100422
- File Encoding         : 65001
 
- Date: 07/05/2023 18:51:18
-*/
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
+--
+-- Base de datos: `elearning_16`
+--
+CREATE DATABASE IF NOT EXISTS `elearning_16` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE `elearning_16`;
 
--- ----------------------------
--- Table structure for attachments
--- ----------------------------
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `attachments`
+--
+
 DROP TABLE IF EXISTS `attachments`;
-CREATE TABLE `attachments`  (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
-  `file_url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+CREATE TABLE `attachments` (
+  `id` bigint(20) NOT NULL,
+  `name` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `file_url` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `course_content` bigint(20) NOT NULL,
-  `create_date` datetime(0) NULL DEFAULT NULL,
-  `create_uid` int(11) NOT NULL,
-  PRIMARY KEY (`id`) USING BTREE,
-  INDEX `attachments.course_content`(`course_content`) USING BTREE,
-  INDEX `attachments.create_uid`(`create_uid`) USING BTREE,
-  CONSTRAINT `attachments.course_content` FOREIGN KEY (`course_content`) REFERENCES `course_content` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `attachments.create_uid` FOREIGN KEY (`create_uid`) REFERENCES `users` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 9 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+  `create_date` datetime DEFAULT NULL,
+  `create_uid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- ----------------------------
--- Table structure for categories
--- ----------------------------
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categories`
+--
+
 DROP TABLE IF EXISTS `categories`;
-CREATE TABLE `categories`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
-  `slug` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
-  `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL,
-  `image_url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
-  `parent_cat` int(255) NULL DEFAULT NULL,
-  `keywords` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
-  `create_uid` int(11) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE,
-  INDEX `create_uid`(`create_uid`) USING BTREE,
-  INDEX `category_parent`(`parent_cat`) USING BTREE,
-  CONSTRAINT `category_parent` FOREIGN KEY (`parent_cat`) REFERENCES `categories` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  CONSTRAINT `create_uid` FOREIGN KEY (`create_uid`) REFERENCES `users` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `slug` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `image_url` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `parent_cat` int(255) DEFAULT NULL,
+  `keywords` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `create_uid` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- ----------------------------
--- Table structure for comments
--- ----------------------------
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `comments`
+--
+
 DROP TABLE IF EXISTS `comments`;
-CREATE TABLE `comments`  (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `course_content` bigint(20) NULL DEFAULT NULL,
-  `user` int(11) NULL DEFAULT NULL,
-  `parent_comment` bigint(20) NULL DEFAULT NULL,
-  `create_date` datetime(0) NULL DEFAULT NULL,
-  `create_uid` int(11) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE,
-  INDEX `user`(`user`) USING BTREE,
-  INDEX `comments.coursecontent`(`course_content`) USING BTREE,
-  INDEX `id`(`id`) USING BTREE,
-  INDEX `comments.parent_comment`(`parent_comment`) USING BTREE,
-  CONSTRAINT `comments.coursecontent` FOREIGN KEY (`course_content`) REFERENCES `course_content` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  CONSTRAINT `comments.parent_comment` FOREIGN KEY (`parent_comment`) REFERENCES `comments` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  CONSTRAINT `user` FOREIGN KEY (`user`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+CREATE TABLE `comments` (
+  `id` bigint(20) NOT NULL,
+  `course_content` bigint(20) DEFAULT NULL,
+  `user` int(11) DEFAULT NULL,
+  `parent_comment` bigint(20) DEFAULT NULL,
+  `create_date` datetime DEFAULT NULL,
+  `create_uid` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- ----------------------------
--- Table structure for course_content
--- ----------------------------
-DROP TABLE IF EXISTS `course_content`;
-CREATE TABLE `course_content`  (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `slug` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL,
-  `iframe` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `thumbnail_url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
-  `course` int(11) NOT NULL,
-  `create_date` datetime(0) NOT NULL,
-  `create_uid` int(11) NOT NULL,
-  PRIMARY KEY (`id`, `slug`) USING BTREE,
-  INDEX `course_content.create_uid`(`create_uid`) USING BTREE,
-  INDEX `id`(`id`) USING BTREE,
-  CONSTRAINT `course_content.create_uid` FOREIGN KEY (`create_uid`) REFERENCES `users` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 8 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+-- --------------------------------------------------------
 
--- ----------------------------
--- Table structure for courses
--- ----------------------------
+--
+-- Estructura de tabla para la tabla `courses`
+--
+
 DROP TABLE IF EXISTS `courses`;
-CREATE TABLE `courses`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
-  `slug` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
-  `description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
-  `thumbnail_url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
-  `category` int(11) NULL DEFAULT NULL,
-  `teacher` int(11) NULL DEFAULT NULL,
-  `keywords` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
-  `create_date` datetime(0) NULL DEFAULT NULL,
-  `create_uid` int(11) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE,
-  INDEX `courses_create_uid`(`create_uid`) USING BTREE,
-  INDEX `courses_teacher`(`teacher`) USING BTREE,
-  INDEX `courses.category`(`category`) USING BTREE,
-  CONSTRAINT `courses.category` FOREIGN KEY (`category`) REFERENCES `categories` (`id`) ON DELETE SET NULL ON UPDATE SET NULL,
-  CONSTRAINT `courses_create_uid` FOREIGN KEY (`create_uid`) REFERENCES `users` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  CONSTRAINT `courses_teacher` FOREIGN KEY (`teacher`) REFERENCES `users` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 56 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+CREATE TABLE `courses` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `slug` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `description` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `thumbnail_url` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `category` int(11) DEFAULT NULL,
+  `teacher` int(11) DEFAULT NULL,
+  `keywords` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `create_date` datetime DEFAULT NULL,
+  `create_uid` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- ----------------------------
--- Table structure for site_config
--- ----------------------------
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `course_content`
+--
+
+DROP TABLE IF EXISTS `course_content`;
+CREATE TABLE `course_content` (
+  `id` bigint(20) NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `slug` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `iframe` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `thumbnail_url` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `course` int(11) NOT NULL,
+  `create_date` datetime NOT NULL,
+  `create_uid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `site_config`
+--
+
 DROP TABLE IF EXISTS `site_config`;
-CREATE TABLE `site_config`  (
-  `variable` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `value` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
-  PRIMARY KEY (`variable`) USING BTREE,
-  UNIQUE INDEX `variable.unique`(`variable`) USING BTREE COMMENT 'Unique variable name'
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+CREATE TABLE `site_config` (
+  `variable` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `value` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- ----------------------------
--- Table structure for users
--- ----------------------------
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
 DROP TABLE IF EXISTS `users`;
-CREATE TABLE `users`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `firstname` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'User Firstname',
-  `lastname` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'User Lastname',
-  `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
-  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'E-Mail for contact and login',
-  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'User Password for login pruposes',
-  `permission_type` enum('admin','teacher','student') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT 'Permission Type Admin, Teacher or Student',
-  `avatar_url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT 'Avatar iamge URL',
-  `active` tinyint(4) NULL DEFAULT NULL COMMENT 'State of user',
-  PRIMARY KEY (`id`, `email`) USING BTREE,
-  UNIQUE INDEX `Id`(`id`) USING BTREE COMMENT 'id',
-  UNIQUE INDEX `Unique email`(`email`) USING BTREE COMMENT 'email',
-  UNIQUE INDEX `Unique username`(`username`) USING BTREE COMMENT 'username'
-) ENGINE = InnoDB AUTO_INCREMENT = 16 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `firstname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'User Firstname',
+  `lastname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'User Lastname',
+  `username` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'E-Mail for contact and login',
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'User Password for login pruposes',
+  `permission_type` enum('admin','teacher','student') COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'Permission Type Admin, Teacher or Student',
+  `avatar_url` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'Avatar iamge URL',
+  `active` tinyint(4) DEFAULT NULL COMMENT 'State of user'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-SET FOREIGN_KEY_CHECKS = 1;
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `attachments`
+--
+ALTER TABLE `attachments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `attachments.course_content` (`course_content`),
+  ADD KEY `attachments.create_uid` (`create_uid`);
+
+--
+-- Indices de la tabla `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `create_uid` (`create_uid`),
+  ADD KEY `category_parent` (`parent_cat`);
+
+--
+-- Indices de la tabla `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`) USING BTREE,
+  ADD KEY `user` (`user`),
+  ADD KEY `comments.coursecontent` (`course_content`),
+  ADD KEY `id` (`id`),
+  ADD KEY `comments.parent_comment` (`parent_comment`);
+
+--
+-- Indices de la tabla `courses`
+--
+ALTER TABLE `courses`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `courses_create_uid` (`create_uid`),
+  ADD KEY `courses_teacher` (`teacher`),
+  ADD KEY `courses.category` (`category`);
+
+--
+-- Indices de la tabla `course_content`
+--
+ALTER TABLE `course_content`
+  ADD PRIMARY KEY (`id`,`slug`) USING BTREE,
+  ADD KEY `course_content.create_uid` (`create_uid`),
+  ADD KEY `id` (`id`);
+
+--
+-- Indices de la tabla `site_config`
+--
+ALTER TABLE `site_config`
+  ADD PRIMARY KEY (`variable`) USING BTREE,
+  ADD UNIQUE KEY `variable.unique` (`variable`) USING BTREE COMMENT 'Unique variable name';
+
+--
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`,`email`) USING BTREE,
+  ADD UNIQUE KEY `Id` (`id`) USING BTREE COMMENT 'id',
+  ADD UNIQUE KEY `Unique email` (`email`) USING BTREE COMMENT 'email',
+  ADD UNIQUE KEY `Unique username` (`username`) USING BTREE COMMENT 'username';
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `attachments`
+--
+ALTER TABLE `attachments`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `courses`
+--
+ALTER TABLE `courses`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `course_content`
+--
+ALTER TABLE `course_content`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `attachments`
+--
+ALTER TABLE `attachments`
+  ADD CONSTRAINT `attachments.course_content` FOREIGN KEY (`course_content`) REFERENCES `course_content` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `attachments.create_uid` FOREIGN KEY (`create_uid`) REFERENCES `users` (`id`);
+
+--
+-- Filtros para la tabla `categories`
+--
+ALTER TABLE `categories`
+  ADD CONSTRAINT `category_parent` FOREIGN KEY (`parent_cat`) REFERENCES `categories` (`id`),
+  ADD CONSTRAINT `create_uid` FOREIGN KEY (`create_uid`) REFERENCES `users` (`id`);
+
+--
+-- Filtros para la tabla `comments`
+--
+ALTER TABLE `comments`
+  ADD CONSTRAINT `comments.coursecontent` FOREIGN KEY (`course_content`) REFERENCES `course_content` (`id`),
+  ADD CONSTRAINT `comments.parent_comment` FOREIGN KEY (`parent_comment`) REFERENCES `comments` (`id`),
+  ADD CONSTRAINT `user` FOREIGN KEY (`user`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `courses`
+--
+ALTER TABLE `courses`
+  ADD CONSTRAINT `courses.category` FOREIGN KEY (`category`) REFERENCES `categories` (`id`) ON DELETE SET NULL ON UPDATE SET NULL,
+  ADD CONSTRAINT `courses_create_uid` FOREIGN KEY (`create_uid`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `courses_teacher` FOREIGN KEY (`teacher`) REFERENCES `users` (`id`);
+
+--
+-- Filtros para la tabla `course_content`
+--
+ALTER TABLE `course_content`
+  ADD CONSTRAINT `course_content.create_uid` FOREIGN KEY (`create_uid`) REFERENCES `users` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
