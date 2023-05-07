f39eb238564b8752a7f638b46b2484bec924f03a
Ayoub Saif <ayoubsaif@gmail.com>
2023-05-07 10:22:38 +0200

    add profile and siteconfig endpoints to router

--- backend/app/routes.php
+++ backend/app/routes.php
@@ -1,8 +1,15 @@
 <?php
 
+# SiteConfig
+$router->get('/api/site-config', 'SiteConfigController@get');
+
 # Auth
 $router->post('/api/login', 'UserController@login');
 $router->post('/api/register', 'UserController@register');
+
+# Profile
+$router->get('/api/profile', 'UserController@getMyProfileInfo');
+$router->put('/api/profile', 'UserController@updateMyProfileInfo');
 
 # Courses
 $router->get('/api/courses', 'CoursesController@getAll');
@@ -20,4 +27,3 @@ $router->get('/api/attachments', 'AttachmentsController@getAll');
 
 $router->post('/api/attachment', 'AttachmentsController@create');
 $router->get('/api/attachment/:id', 'AttachmentsController@getOne');
-
