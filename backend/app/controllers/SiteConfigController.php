008898235cc119d676bb251cec4dac6f3ce134e7
Ayoub Saif <ayoubsaif@gmail.com>
2023-05-13 11:56:41 +0200

    update siteConfig structure to variable>value

--- backend/app/controllers/SiteConfigController.php
+++ backend/app/controllers/SiteConfigController.php
@@ -8,9 +8,10 @@ class SiteConfigController
     function get() {
         try{
             $siteConfig = new SiteConfigModel();
-            if ($siteConfig->get()) {
+            $siteConfigArray = $siteConfig->get();
+            if ($siteConfigArray) {
                 http_response_code(200);
-                echo json_encode($siteConfig);
+                echo json_encode($siteConfigArray);
                 return;
             } else {
                 http_response_code(500);
@@ -20,6 +21,43 @@ class SiteConfigController
         } catch (Exception $e) {
             http_response_code(401);
             echo json_encode(array("message" => "Unauthorized"));
+        }
+    }
+
+    function update(){
+        try{
+            $PermissionMiddleware = new PermissionMiddleware();
+            $allowed = array('admin');
+            $UserPermmited = $PermissionMiddleware->handle($allowed);
+            if (!$UserPermmited) {
+                return;
+            }
+            $data = json_decode(file_get_contents("php://input"));
+            $update_state = [];
+            foreach ($data as $key => $value) {
+                $siteConfig = new SiteConfigModel();
+                $siteConfig->variable = $key;
+                $siteConfig->value = strval($value);
+                if ($siteConfig->update()) {
+                    array_push($update_state, array("status" => "success","message" => "{$key} was updated"));
+                } else {
+                    array_push($update_state, array("status" => "error", "message" => "Unable to update site config"));
+                }
+            }
+
+            if (!empty($update_state)) {
+                http_response_code(200);
+                echo json_encode($update_state);
+                return;
+            }else{
+                http_response_code(500);
+                echo json_encode(array("message" => "Unable to update site config"));
+                return;
+            }
+
+        } catch (Exception $e) {
+            http_response_code(401);
+            echo json_encode(array("message" => "Unauthorized {$e->getMessage()}"));
         }
     }
 
--- backend/app/models/SiteConfigModel.php
+++ backend/app/models/SiteConfigModel.php
@@ -6,63 +6,67 @@ class siteConfigModel
 {
     private $conn;
 
-    public $id;
-    public $hero_action;
-    public $menu;
-    public $create_uid;
-    public $logo_url;
+    public $variable;
+    public $value;
 
     public function __construct()
     {
         $this->conn = Connection::connectDB();
     }
 
-    function update()
+    function create()
     {
-        $query = "UPDATE site_config
-            SET
-                hero_action=:hero_action,
-                menu=:menu,
-                create_uid=:create_uid,
-                logo_url=:logo_url";
-
+        $query = "INSERT INTO site_config SET variable = :variable, value = :value";
         $stmt = $this->conn->prepare($query);
-        $this->hero_action = htmlspecialchars(strip_tags($this->hero_action));
-        $this->menu = htmlspecialchars(strip_tags($this->menu));
-        $this->create_uid = htmlspecialchars(strip_tags($this->create_uid));
-        $this->logo_url = htmlspecialchars(strip_tags($this->logo_url));
 
-        $stmt->bindParam(":hero_action", $this->hero_action);
-        $stmt->bindParam(":menu", $this->menu);
-        $stmt->bindParam(":create_uid", $this->create_uid);
-        $stmt->bindParam(":logo_url", $this->logo_url);
+        $stmt->bindParam(':variable', $this->variable);
+        $stmt->bindParam(':value', $this->value);
 
         if ($stmt->execute()) {
-            return true;
-        }
-
-        return false;
-    }
-
-    function get()
-    {
-        $query = "SELECT * FROM site_config LIMIT 1";
-        $stmt = $this->conn->prepare($query);
-
-        # check if query return data or not
-        if ($stmt->execute() && $stmt->rowCount() > 0) {
-            $row = $stmt->fetch(PDO::FETCH_ASSOC);
-            $this->id = $row['id'];
-            $this->hero_action = $row['hero_action'];
-            $this->menu = $row['menu'];
-            $this->create_uid = $row['create_uid'];
-            $this->logo_url = $row['logo_url'];
             return true;
         } else {
             return false;
         }
+    }
 
-        return false;
+    function update()
+    {
+        $query = "SELECT COUNT(*) FROM site_config WHERE variable = :variable";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(':variable', $this->variable);
+        $stmt->execute();
+        $count = $stmt->fetchColumn();
+    
+        if ($count == 0) {
+            return $this->create();
+        } else {
+            $query = "UPDATE site_config SET value = :value WHERE variable = :variable";
+            $stmt = $this->conn->prepare($query);
+            $stmt->bindParam(':variable', $this->variable);
+            $stmt->bindParam(':value', $this->value);
+            if ($stmt->execute()) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    function get()
+    {
+        $query = "SELECT * FROM site_config";
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+        $siteConfig = $stmt->fetchAll(PDO::FETCH_OBJ);
+        $siteConfigArray = array();
+        foreach ($siteConfig as $config) {
+            $siteConfigArray[$config->variable] = $config->value;
+        }
+        if ($siteConfig) {
+            return $siteConfigArray;
+        } else {
+            return false;
+        }
     }
     
 }
