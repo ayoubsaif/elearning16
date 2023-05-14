b9621a32f8c744a35956ee6a92b153373f1a8a2f
Ayoub Saif <ayoubsaif@gmail.com>
2023-05-14 19:45:36 +0200

    add input component

---
+++ frontend/src/components/forms/Input.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Input } from "@chakra-ui/react";
+
+export default function PrimaryInput({ attributes }) {
+  return (
+    <Input
+        {...attributes}
+        rounded={"sm"}
+        border={"1px"}
+        borderColor="black"
+        _hover={{
+            borderColor: "black",
+            boxShadow: "0 0 0 1px blue.300",
+        }}
+        _focusWithin={{
+            outline: "2px solid",
+            outlineColor: "blue.100",
+            outlineOffset: "0px",
+        }}
+    />
+  )
+}
