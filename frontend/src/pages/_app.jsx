8f656f84109d6f85bc9d390a407450ba4a6e838b
Ayoub Saif <ayoubsaif@gmail.com>
2023-05-14 19:46:02 +0200

    add blue colors for app theme

--- frontend/src/pages/_app.jsx
+++ frontend/src/pages/_app.jsx
@@ -1,17 +1,19 @@
 // pages/_app.js
 import { ChakraProvider } from '@chakra-ui/react'
-
-// 1. Import the extendTheme function
 import { extendTheme } from '@chakra-ui/react'
+import "@/styles/globals.css"
 
-// 2. Extend the theme to include custom colors, fonts, etc
 const colors = {
-  brand: {
-    400: '#8f00ff',
-    900: '#1a365d',
-    800: '#153e75',
-    700: '#2a69ac',
-  },
+  blue: {
+    50: '#cbe0f3',
+    100: '#98c1e7',
+    200: '#65a1da',
+    300: '#3182ce',
+    400: '#1f5181',
+    500: '#194167',
+    600: '#12314d',
+    800: '#06101a',
+  }
 }
 
 export const theme = extendTheme({ colors })
