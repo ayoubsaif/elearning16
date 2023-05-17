e05189c66b0df74e6b479be940c971879fc29073
Ayoub Saif <ayoubsaif@gmail.com>
2023-05-17 23:56:12 +0200

    move files to correct folders

---
+++ frontend/src/pages/auth/login.jsx
@@ -0,0 +1,114 @@
+import {
+  Checkbox,
+  Flex,
+  FormControl,
+  FormLabel,
+  Heading,
+  Link,
+  Stack,
+  Image,
+  useColorModeValue,
+  FormErrorMessage,
+} from "@chakra-ui/react";
+import { useEffect, useRef } from "react";
+import { signIn } from "next-auth/react";
+import { Formik, Form, Field } from "formik";
+
+import Button from "@/components/forms/Button";
+import Input from "@/components/forms/Input";
+
+export default function Login() {
+  const email = useRef("");
+  const password = useRef("");
+
+  const validateEmail = (value) => {
+    let error;
+    if (!value) {
+      error = "Correo electrónico requerido";
+    } else if (
+      !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value) && value !== " "
+    ) {
+      error = "Correo electrónico inválido";
+    }
+    return error;
+  };
+
+  return (
+    <Stack minH={"100vh"} direction={{ base: "column", md: "row" }}>
+      <Flex p={8} flex={1} align={"center"} justify={"center"}>
+        <Stack spacing={4} w={"full"} maxW={"md"}>
+          <Heading fontSize={"2xl"}>Bienvenido de nuevo</Heading>
+          <Formik
+            initialValues={{ email: '', password: '' }}
+            onSubmit={async (values, actions) => {
+              await signIn("credentials", {
+                email: values.email,
+                password: values.password,
+                redirect: true,
+                callbackUrl: "/courses",
+              });
+              actions.setSubmitting(false);
+            }}
+          >
+            {(props) => (
+              <Form>
+                <Field name="email" validate={validateEmail}>
+                  {({ field, form }) => (
+                    <FormControl isInvalid={form.errors.email && form.touched.email}>
+                      <FormLabel>Correo electrónico</FormLabel>
+                      <Input
+                        {...field}
+                      />
+                      <FormErrorMessage>{form.errors.email}</FormErrorMessage>
+                    </FormControl>
+                  )}
+                </Field>
+                
+                <Field name="password">
+                  {({ field, form }) => (
+                    <FormControl isInvalid={form.errors.password && form.touched.password}>
+                      <FormLabel>Contraseña</FormLabel>
+                      <Input
+                        {...field}
+                        type="password"
+                      />
+                      <FormErrorMessage>{form.errors.password}</FormErrorMessage>
+                    </FormControl>
+                  )}
+                </Field>
+                <Stack spacing={6}>
+                  <Stack
+                    direction={{ base: "column", sm: "row" }}
+                    align={"start"}
+                    justify={"space-between"}
+                  ></Stack>
+                  <Button
+                    bg="black"
+                    color="white"
+                    _hover={{
+                      color: "white",
+                      bg: "blue.300",
+                    }}
+                    isLoading={props.isSubmitting}
+                    type="submit"
+                  >
+                    Iniciar Sesión
+                  </Button>
+                </Stack>
+              </Form>
+            )}
+          </Formik>
+        </Stack>
+      </Flex>
+      <Flex flex={1}>
+        <Image
+          alt={"Login Image"}
+          objectFit={"cover"}
+          src={
+            "https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80"
+          }
+        />
+      </Flex>
+    </Stack>
+  );
+}
--- frontend/src/pages/course/[slug].jsx
+++ frontend/src/pages/course/[slug].jsx
@@ -1,11 +1,9 @@
 import Head from 'next/head'
-import Image from 'next/image'
+import { useRouter } from 'next/router'
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
 
-const inter = Inter({ subsets: ['latin'] })
-
-export default function Home() {
+export default function Home({ params }) {
+  const router = useRouter()
   return (
     <>
       <Head>
@@ -14,11 +12,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>
-        <div className={styles.description}>
-            <h1>CREATE COURSE PAGE</h1>
-        </div>
-      </main>
+      <div>
+          <h1>Course Page: {router.query.slug}</h1>
+      </div>
     </>
   )
 }
