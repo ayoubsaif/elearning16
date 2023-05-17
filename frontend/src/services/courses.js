89385aba79661cca651510c6ec04955831d9e160
Ayoub Saif <ayoubsaif@gmail.com>
2023-05-17 23:55:41 +0200

    refactor url from env and remove console.log

--- frontend/src/services/courses.js
+++ frontend/src/services/courses.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 
-const API_URL = process.env.NEXT_APP_API_HOST ? process.env.NEXT_APP_API_HOST : "http://localhost";
+const API_URL = process.env.NEXT_APP_API_HOST || "http://localhost";
 
 export async function getCourses(currentPage) {
     try {
         const res = await axios.get(`${API_URL}/api/courses`, { params: { currentPage } });
-        console.log(res.data);
         return res.data;
     } catch (error) {
         console.error("Error fetching courses:", error);
