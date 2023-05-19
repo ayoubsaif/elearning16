07a4d3b8e6f40eb12fc230cb1e83852b75e2f140
Ayoub Saif <ayoubsaif@gmail.com>
2023-05-20 01:08:56 +0200

    add OPTIONS to header CORS

--- backend/index.php
+++ backend/index.php
@@ -7,7 +7,7 @@ ini_set("log_errors", 1);
 ini_set("error_log", "./logs/error.log");
 
 header('Access-Control-Allow-Origin: *');
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header('Content-Type: application/json; charset=utf-8');
 
