8b04536012019eee32565e4772b6a6182e235229
Ayoub Saif <ayoubsaif@gmail.com>
2023-05-21 00:07:45 +0200

    refactor courses service

--- frontend/src/services/courses.js
+++ frontend/src/services/courses.js
@@ -2,14 +2,19 @@ import axios from "axios";
 
 const API_URL = process.env.NEXT_APP_API_HOST || "http://localhost";
 
-export async function getCourses(currentPage) {
-    try {
-        const res = await axios.get(`${API_URL}/api/courses`, { params: { currentPage } });
-        return res.data;
-    } catch (error) {
-        console.error("Error fetching courses:", error);
-        throw error;
-    }
+export async function getCourses(accessToken, params) {
+  try {
+    let options = {
+      params: {
+        ...params,
+      },
+    };
+    const res = await axios.get(`${API_URL}/api/courses`, {...options});
+    return res.data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 }
 
 export function getCourse(slug) {
