350b7c8981a7f58042ca06756a967b91ce47ce64
Ayoub Saif <ayoubsaif@gmail.com>
2023-05-23 23:26:37 +0200

    reduce footer layout

--- frontend/src/layout/Footer.jsx
+++ frontend/src/layout/Footer.jsx
@@ -31,7 +31,7 @@ const Logo = (props) => {
   );
 };
 
-const SocialButton = ({children,label,href}) => {
+const SocialButton = ({ children, label, href }) => {
   return (
     <chakra.button
       bg={useColorModeValue("blackAlpha.100", "whiteAlpha.100")}
@@ -55,57 +55,33 @@ const SocialButton = ({children,label,href}) => {
   );
 };
 
-export default function SmallCentered() {
+export default function Footer({ siteConfig }) {
   return (
-    <Box
-      bg={useColorModeValue("gray.50", "gray.900")}
-      color={useColorModeValue("gray.700", "gray.200")}
-    >
+    <Box color={"black"} borderTop={"1px solid black"}>
       <Container
         as={Stack}
         maxW={"6xl"}
         py={4}
+        direction={{ base: "column", md: "row" }}
         spacing={4}
-        justify={"center"}
-        align={"center"}
+        justify={{ base: "center", md: "space-between" }}
+        align={{ base: "center", md: "center" }}
       >
-        <Logo />
+        <Text>
+          © {new Date().getFullYear()} {siteConfig?.title}
+        </Text>
         <Stack direction={"row"} spacing={6}>
-          <Link href={"#"}>Home</Link>
-          <Link href={"#"}>About</Link>
-          <Link href={"#"}>Blog</Link>
-          <Link href={"#"}>Contact</Link>
+          <SocialButton label={"Twitter"} href={"#"}>
+            <FaTwitter />
+          </SocialButton>
+          <SocialButton label={"YouTube"} href={"#"}>
+            <FaYoutube />
+          </SocialButton>
+          <SocialButton label={"Instagram"} href={"#"}>
+            <FaInstagram />
+          </SocialButton>
         </Stack>
       </Container>
-
-      <Box
-        borderTopWidth={1}
-        borderStyle={"solid"}
-        borderColor={useColorModeValue("gray.200", "gray.700")}
-      >
-        <Container
-          as={Stack}
-          maxW={"6xl"}
-          py={4}
-          direction={{ base: "column", md: "row" }}
-          spacing={4}
-          justify={{ base: "center", md: "space-between" }}
-          align={{ base: "center", md: "center" }}
-        >
-          <Text>© 2022 Chakra Templates. All rights reserved</Text>
-          <Stack direction={"row"} spacing={6}>
-            <SocialButton label={"Twitter"} href={"#"}>
-              <FaTwitter />
-            </SocialButton>
-            <SocialButton label={"YouTube"} href={"#"}>
-              <FaYoutube />
-            </SocialButton>
-            <SocialButton label={"Instagram"} href={"#"}>
-              <FaInstagram />
-            </SocialButton>
-          </Stack>
-        </Container>
-      </Box>
     </Box>
   );
 }
