a68b38cced696dbd9c6827ebc340fd3b2bc1c7ab
Ignacio Tomás <ignaciotomasico12@gmail.com>
2023-05-25 23:09:25 +0200

    imporve the create course form

--- frontend/src/pages/course/create.jsx
+++ frontend/src/pages/course/create.jsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import { NextSeo } from 'next-seo';
 import * as Yup from "yup";
 import Layout from "@/layout/Layout";
+import { BsPlusCircle, BsPencilSquare } from "react-icons/bs";
 import {
   Flex,
   Box,
@@ -14,9 +15,15 @@ import {
   Select,
   FormLabel,
   Textarea,
-  Button
+  Button,
+  Center,
+  InputRightElement,
+  Image,
+  IconButton
 } from '@chakra-ui/react';
+import ChakraTagInput from "@/components/forms/ChakraTagInput";
 import Input from "@/components/forms/input";
+import CropImageModal from "@/components/modals/cropImageModal";
 import { useFormik } from "formik";
 import { createCourse } from "@/services/courses";
 import { getSiteConfig } from "@/services/siteConfig";
@@ -27,15 +34,27 @@ import { getServerSession } from "next-auth/next";
 
 
 export default function CreateCourse(props) {
-  const { siteConfig, menuItems } = props;
-  const [course, setCourse] = useState(null);
+  const { siteConfig, menuItems, categories } = props;
+  const [course, setCourse] = useState({...Box, image: '/img/empty_course.png'});
+  const [tags, setTags] = useState([]);
+  const [selectedImageFile, setSelectedImageFile] = useState(null);
+
+  const handleTagsChange = useCallback((event, tags) => {
+    setTags(tags)
+  }, [])
+
+  const generateSlug = (name) => {
+    let slug = name.toLowerCase().replace(/ /g, "-").replace(/[^\w-]+/g, "")
+    formik.setFieldValue("slug", slug);
+  }
 
   const formik = useFormik({
     initialValues: {
       name: course?.name || "",
+      slug: course?.slug || "",
       description: course?.description || "",
       category: course?.category || "",
-      keywords: course?.keywords || "",
+      keywords: null,
       thumbnail_url: null,
     },
     validationSchema: Yup.object({
@@ -54,7 +73,7 @@ export default function CreateCourse(props) {
         formData.append("name", values.name);
         formData.append("description", values.description);
         formData.append("category", values.category);
-        formData.append("keywords", values.keywords);
+        formData.append("keywords", tags);
 
         const createCourse = await createCourse(
           formData,
@@ -64,6 +83,7 @@ export default function CreateCourse(props) {
           setCourse({
             ...course,
             name: createCourse?.name,
+            slug: createCourse?.slug,
             description: createCourse?.description,
             category: createCourse?.category,
             keywords: createCourse?.keywords,
@@ -84,6 +104,27 @@ export default function CreateCourse(props) {
     },
   });
 
+  const handleImageChange = (e) => {
+    e.preventDefault();
+    setSelectedImageFile(e.target.files[0]);
+    handleImageSave(e.target.files[0]);
+  };
+
+  const handleImageSave = (croppedImage) => {
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      const imageSrc = e.target.result;
+      console.log(imageSrc);
+      setCourse((prevProfile) => ({
+        ...prevProfile,
+        image: imageSrc,
+      }));
+    };
+    reader.readAsDataURL(croppedImage);
+    formik.setFieldValue("thumbnail_url", croppedImage);
+    setSelectedImageFile(null);
+  };
+
   return (
     <>
       <NextSeo
@@ -98,8 +139,8 @@ export default function CreateCourse(props) {
       />
       <Layout siteConfig={siteConfig} menuItems={menuItems}>
         <Flex
-          minH={'100vh'}
-          align={'center'}
+          minH={'100%'}
+          align={'start'}
           justify={'center'}
           bgSize="cover"
           bgPosition="center">
@@ -119,7 +160,7 @@ export default function CreateCourse(props) {
               >
                 <form onSubmit={formik.handleSubmit}>
                   <Stack spacing={8}>
-                    <FormControl>
+                    <FormControl isRequired>
                       <FormLabel>Nombre del Curso</FormLabel>
                       <Input
                         id="name"
@@ -130,33 +171,143 @@ export default function CreateCourse(props) {
                         {formik.errors.firstname}
                       </FormErrorMessage>
                     </FormControl>
+                    <FormControl isRequired>
+                      <FormLabel>Slug del Curso</FormLabel>
+                      <InputGroup
+                        style={{ paddingRight: "5.5rem" }}
+                      >
+                        <Input
+                          id="slug"
+                          name="slug"
+                          {...formik.getFieldProps("slug")}
+                        />
+                        <InputRightElement width='4.5rem'>
+                          <Button onClick={() => generateSlug(formik.values.name)} variant="primary">
+                            Generar
+                          </Button>
+                        </InputRightElement>
+                      </InputGroup>
+                      <FormErrorMessage>
+                        {formik.errors.firstname}
+                      </FormErrorMessage>
+                    </FormControl>
                     <FormControl id="description" isRequired>
                       <FormLabel>Descripción del Curso</FormLabel>
                       <Textarea type="text" placeholder='Descripción..'
                         id="descripcion"
                         name="descripcion"
                         style={{ width: "100%" }}
+                        rounded={".25em"}
+                        border={"1px"}
+                        borderColor="gray.300"
+                        _hover={{
+                            borderColor: "black",
+                            boxShadow: "0 0 0 1px blue.300",
+                        }}
+                        _focusWithin={{
+                            outline: "2px solid",
+                            outlineColor: "blue.100",
+                            outlineOffset: "0px",
+                        }}
                         {...formik.getFieldProps("descripcion")}
                       />
                     </FormControl>
 
                     <FormControl id="category" isRequired>
-                      <Select placeholder="Categoría">
-                      
+                      <Select placeholder='Selecciona una categoría'
+                        style={{ width: "100%" }}
+                        rounded={".25em"}
+                        border={"1px"}
+                        borderColor="gray.300"
+                        _hover={{
+                            borderColor: "black",
+                            boxShadow: "0 0 0 1px blue.300",
+                        }}
+                        _focusWithin={{
+                            outline: "2px solid",
+                            outlineColor: "blue.100",
+                            outlineOffset: "0px",
+                        }}
+                      >
+                        {categories?.map((category) => (
+                          <option key={category.id} value={category.id}>
+                            {category.name}
+                          </option>
+                        ))}
                       </Select>
                     </FormControl>
 
-                    <FormControl id="labels" isRequired>
-                      <InputGroup>
-                        <Input type="text" placeholder='Etiquetas' />
-                      </InputGroup>
-                    </FormControl>
-                    <FormControl id="thumbnail" isRequired>
-                      <FormLabel>Elige una miniatura</FormLabel>
-                      <InputGroup>
-                        <Input type="file" />
-                      </InputGroup>
-                    </FormControl>
+                    <Box>
+                      <FormControl id="keywords" isRequired>
+                        <FormLabel>Etiquetas del Curso</FormLabel>
+                        <ChakraTagInput
+                          style={{ width: "100%" }}
+                          rounded={".25em"}
+                          border={"1px"}
+                          borderColor="gray.300"
+                          _hover={{
+                              borderColor: "black",
+                              boxShadow: "0 0 0 1px blue.300",
+                          }}
+                          _focusWithin={{
+                              outline: "2px solid",
+                              outlineColor: "blue.100",
+                              outlineOffset: "0px",
+                          }}
+                          tags={tags}
+                          onTagsChange={handleTagsChange}
+                        />
+                      </FormControl>
+                    </Box>
+                    <Stack spacing={10}>
+                      <HStack>
+                        <Box textAlign="center" position="relative">
+                          <Image
+                            size="xl"
+                            name={course?.name}
+                            src={course?.image}
+                            width={270}
+                            height={150}
+                            objectFit="cover"
+                            borderRadius="md"
+                          />
+                          <FormControl isInvalid={formik.errors.image}>
+                            <Box
+                              position="absolute"
+                              inset={-2}
+                              display="flex"
+                              alignItems="end"
+                              justifyContent="end"
+                            >
+                              <input
+                                id="thumbnail_url"
+                                type="file"
+                                accept=".jpg,.jpeg,.png"
+                                onChange={(e) => {
+                                  handleImageChange(e);
+                                  formik.setFieldValue(
+                                    "thumbnail_url",
+                                    e.currentTarget.files[0]
+                                  );
+                                }}
+                                style={{ display: "none" }}
+                              />
+                              <IconButton
+                                icon={<BsPencilSquare />}
+                                aria-label="Change course Image"
+                                rounded={"full"}
+                                onClick={() =>
+                                  document.getElementById("thumbnail_url").click()
+                                }
+                              />
+                            </Box>
+                            <FormErrorMessage>
+                              {formik.errors.avatar}
+                            </FormErrorMessage>
+                          </FormControl>
+                        </Box>
+                      </HStack>
+                    </Stack>
 
                     {/* Añadir Usuarios
                     <Box>
@@ -186,16 +337,12 @@ export default function CreateCourse(props) {
 
                     <Stack spacing={10} pt={2}>
                       <Button
-                        bg="black"
-                        color="white"
-                        _hover={{
-                          color: "white",
-                          bg: "blue.300",
-                          transform: "translate(-.25rem, -.25rem)",
-                          boxShadow: ".25rem .25rem 0 black",
-                        }}
+                        leftIcon={<BsPlusCircle />}
+                        variant="primary"
                       >
-                        Crear curso
+                        <Center>
+                          Crear curso
+                        </Center>
                       </Button>
                     </Stack>
                   </Stack>
@@ -219,11 +366,28 @@ export async function getServerSideProps(context) {
       },
     };
   }
+
+  const categories = [
+    {
+      id: 1,
+      name: "Programación",
+    },
+    {
+      id: 2,
+      name: "Diseño",
+    },
+    {
+      id: 3,
+      name: "Marketing",
+    },
+  ]
+
   const siteConfig = await getSiteConfig();
   const menuItems = await getMenuItems(session?.user?.accessToken);
   const profileInfo = await getProfile(session?.user?.accessToken);
   return {
     props: {
+      categories,
       siteConfig,
       menuItems,
       profileInfo,
