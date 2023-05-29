74f2fa8e0d7ce2dc4797df7c29e94a0a65346754
Ayoub Saif <ayoubsaif@gmail.com>
2023-05-29 22:58:13 +0200

    optimize coursetoolbar to search bar and create course button

--- frontend/src/components/dataDisplay/coursesToolbar.jsx
+++ frontend/src/components/dataDisplay/coursesToolbar.jsx
@@ -1,71 +1,38 @@
-        
-import React, { useRef } from "react"
-import {
-    HStack,
-    Menu,
-    MenuButton,
-    MenuList,
-    MenuItemOption ,
-    IconButton,
-    Button,
-    MenuOptionGroup,
-    Tooltip
-} from "@chakra-ui/react";
-import { BsFunnel, BsPlusLg, BsX } from "react-icons/bs";
+import React from "react";
+import { Button, Flex, Box, Spacer, Text, Divider } from "@chakra-ui/react";
 import Link from "next/link";
-import { useRouter } from "next/router";
 import SearchBar from "@/components/forms/searchInput";
+import { BsPlusLg } from "react-icons/bs";
 
-export default function CoursesToolbar({categories, category, canCreate, searchBar, setSearchBar}) {
-    const router = useRouter();
-    const searchBarRef = useRef(null);
-
-    const resetSearch = () => {
-        setSearchBar(null);
-        searchBarRef.current.setInputValue('');
-    }
-
-    return (
-        <HStack spacing='24px' width={'100%'} justify={'space-between'}>
-            <HStack spacing='24px'>
-                <SearchBar searchBar={searchBar} setSearchBar={setSearchBar} ref={searchBarRef} />
-                <Menu>
-                    <Tooltip label='Filtrar' placement='bottom'>
-                        <MenuButton as={IconButton} icon={<BsFunnel />} />
-                    </Tooltip>
-                    <MenuList>
-                        <MenuOptionGroup defaultValue={category ? category?.slug : null} title='' type='radio'>
-                            {categories.map((cat) => (
-                                <MenuItemOption value={cat?.slug} key={cat?.id} onClick={() => router.push(cat?.name === category?.name ? '/courses' : `/courses/${cat?.slug}`)}>
-                                    {cat?.name}
-                                </MenuItemOption>
-                            ))}
-                        </MenuOptionGroup>
-                    </MenuList>
-                </Menu>
-                {(category || searchBar) &&
-                    <Tooltip label='Eliminar filtros' placement='bottom'>
-                        <IconButton
-                            aria-label="delete-filter"
-                            icon={<BsX />}
-                            onClick={resetSearch}
-                        />
-                    </Tooltip>
-                }
-            </HStack>
-            {canCreate && 
-                <HStack spacing='24px'>
-                    <Link href="/course/create">
-                        <Button
-                            leftIcon={<BsPlusLg />}
-                            variant="primary"
-                            type="submit"
-                        >
-                            Crear curso
-                        </Button>
-                    </Link>
-                </HStack>
-            }
-        </HStack>
-    )
+export default function CoursesToolbar({ canCreate, searchBar, setSearchBar, totalItems }) {
+  return (
+    <Box w="full">
+      <Flex
+        alignItems="center"
+        gap="2"
+        width="full"
+        direction={{ base: "column", md: "row" }}
+      >
+        <Box>
+          <SearchBar searchBar={searchBar} setSearchBar={setSearchBar} />
+        </Box>
+        {canCreate && (
+          <>
+            <Spacer />
+            <Box>
+              <Link href="/course/create">
+                <Button leftIcon={<BsPlusLg />} variant="primary" type="submit">
+                  Crear curso
+                </Button>
+              </Link>
+            </Box>
+          </>
+        )}
+      </Flex>
+      <Text fontSize="md" w="full" color={"gray.500"} mt={2}>
+        {totalItems} { totalItems == 1? "curso encontrado" : "cursos encontrados" }
+      </Text>
+      <Divider my={2} />
+    </Box>
+  );
 }
