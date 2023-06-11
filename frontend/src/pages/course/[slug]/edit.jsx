00af0a1efc6551ebcf415a934ad333579792afd2
Ayoub <ayoubsaif@gmail.com>
2023-06-11 17:19:56 +0200

    fix layout and add drag and drop image feature

--- frontend/src/pages/course/[slug]/edit.jsx
+++ frontend/src/pages/course/[slug]/edit.jsx
@@ -20,6 +20,7 @@ import {
   Image,
   IconButton,
   AspectRatio,
+  Text,
 } from "@chakra-ui/react";
 import ChakraTagInput from "@/components/forms/ChakraTagInput";
 import Input from "@/components/forms/input";
@@ -40,15 +41,16 @@ export default function EditCourse(props) {
   });
   const [tags, setTags] = useState(course?.keywords || []);
   const { data: session } = useSession();
+  const [isDraggingOver, setIsDraggingOver] = useState(false);
 
   const formik = useFormik({
     initialValues: {
       name: course?.name || "",
       slug: course?.slug || "",
       description: course?.description || "",
-      category: course?.category || "",
+      category: course?.category?.id || "",
       keywords: tags,
-      thumbnail: course?.thumbnail || null,
+      thumbnail: null,
     },
     validationSchema: Yup.object({
       name: Yup.string().required("Nombre es obligatorio"),
@@ -108,6 +110,22 @@ export default function EditCourse(props) {
     formik.setFieldValue("thumbnail", file);
   };
 
+  const handleDrop = (e) => {
+    e.preventDefault();
+    const file = e.dataTransfer.files[0];
+    const reader = new FileReader();
+
+    reader.onload = (e) => {
+      const imageSrc = e.target.result;
+      setCourse((prevProfile) => ({
+        ...prevProfile,
+        thumbnail: imageSrc,
+      }));
+    };
+    reader.readAsDataURL(file);
+    formik.setFieldValue("thumbnail", file);
+  };
+
   return (
     <>
       <NextSeo
@@ -121,188 +139,206 @@ export default function EditCourse(props) {
         }}
       />
       <Layout siteConfig={siteConfig} menuItems={menuItems}>
-        <HStack
+        <Heading
+          as="h3"
+          fontSize="lg"
+          textAlign={"center"}
+          color={"black"}
+          my={10}
+        >
+          Editar curso
+        </Heading>
+        <Box
           w={"full"}
           mx="auto"
+          rounded={"md"}
           border={"1px"}
           borderColor={"black"}
-          rounded={"md"}
-          my={5}
-          p={{base: 5, md: 10}}
+          mb={5}
+          p={{ base: 5, md: 10 }}
         >
-          <Box w="full" mx="auto" rounded="md">
-            <Heading
-              as="h3" fontSize="lg"
-              textAlign={"center"}
-              color={"black"}
-              mb={10}
-            >
-              Editar curso
-            </Heading>
-
-            <form onSubmit={formik.handleSubmit}>
-              <Stack spacing={8}>
-                <Box display={{ md: "flex" }} gap={5}>
-                  <FormControl 
-                    width={{ md: 80 }} 
+          <form onSubmit={formik.handleSubmit}>
+            <Stack spacing={8}>
+              <Box display={{ md: "flex" }} gap={5}>
+                <FormControl
+                  width={{ md: 80 }}
+                  flexShrink={0}
+                  mb={{ base: 5, md: 0 }}
+                >
+                  <AspectRatio
+                    ratio={16 / 9}
                     flexShrink={0}
-                    mb={{ base: 5, md: 0 }}
+                    overflow="hidden"
+                    rounded="md"
+                    border={isDraggingOver ? "2px dashed black" : "2px solid transparent"}
+                  >
+                    <Box
+                      textAlign="center"
+                      position="relative"
+                      onDrop={handleDrop}
+                      onDragOver={(e) => {
+                        e.preventDefault();
+                        setIsDraggingOver(true);
+                      }}
+                      onDragLeave={() => setIsDraggingOver(false)}
                     >
-                    <AspectRatio
-                      ratio={16 / 9}
-                      flexShrink={0}
-                      overflow="hidden"
-                      rounded="md"
-                    >
-                      <Box
-                        textAlign="center"
-                        position="relative"
-                      >
-                        {course.thumbnail ? (
-                          <Image
-                            name={course?.name}
-                            src={course?.thumbnail}
-                            bg="black"
-                            objectFit="cover"
-                            borderRadius="md"
-                          />
-                        ) : (
-                          <Box bg="gray.200" height="full" w="full" />
-                        )}
-                        <Box
-                          position="absolute"
-                          inset={4}
-                          display="flex"
-                          alignItems="end"
-                          justifyContent="end"
-                        >
-                          <label htmlFor="thumbnail">
-                            <input
-                              id="thumbnail"
-                              name="thumbnail"
-                              style={{ display: "none" }}
-                              type="file"
-                              accept=".jpg,.jpeg,.png"
-                              onChange={handleImageChange}
-                            />
-                            <IconButton
-                              icon={<BsPencilSquare />}
-                              aria-label="Change course Image"
-                              rounded="full"
-                              variant="outlined"
-                              onClick={() =>
-                                document.getElementById("thumbnail").click()
-                              }
-                            />
-                          </label>
-                        </Box>
-                        <FormErrorMessage>
-                          {formik.errors.thumbnail}
-                        </FormErrorMessage>
-                      </Box>
-                    </AspectRatio>
-                    <FormHelperText>
-                      Imagen del curso (Aspect Ratio 16/9)
-                    </FormHelperText>
-                  </FormControl>
-
-                  <VStack w="full">
-                    <FormControl>
-                      <FormLabel>Nombre del Curso</FormLabel>
-                      <Input
-                        id="name"
-                        name="name"
-                        {...formik.getFieldProps("name")}
-                      />
-                      <FormErrorMessage>{formik.errors.name}</FormErrorMessage>
-                    </FormControl>
-
-                    <FormControl>
-                      <FormLabel>Slug del Curso</FormLabel>
-                      <HStack>
-                        <Input
-                          id="slug"
-                          name="slug"
-                          {...formik.getFieldProps("slug")}
+                      {course.thumbnail ? (
+                        <Image
+                          name={course?.name}
+                          src={course?.thumbnail}
+                          bg="black"
+                          objectFit="cover"
+                          borderRadius="md"
+                          alt={course?.name}
                         />
-                        <Button
-                          onClick={() => generateSlug(formik.values.name)}
-                          variant="primary"
-                        >
-                          Generar
-                        </Button>
-                      </HStack>
-                      <FormErrorMessage>{formik.errors.slug}</FormErrorMessage>
-                    </FormControl>
-
-                    <FormControl>
-                      <FormLabel>Categoría</FormLabel>
-                      <Select
-                        placeholder="Selecciona una categoría"
-                        id="category"
-                        name="category"
-                        {...formik.getFieldProps("category")}
+                      ) : (
+                        <Box bg="gray.200" height="full" w="full" />
+                      )}
+                      <Box
+                        position="absolute"
+                        inset={4}
+                        display="flex"
+                        alignItems="end"
+                        justifyContent="end"
+                        bg={isDraggingOver ? "rgba(0,0,0,0.5)" : "blackAlpha.100"}
                       >
-                        {categories?.map((category) => (
-                          <option key={category.id} value={category.id}>
-                            {category.name}
-                          </option>
-                        ))}
-                      </Select>
-                      <FormErrorMessage>
-                        {formik.errors.category}
-                      </FormErrorMessage>
-                    </FormControl>
-                  </VStack>
-                </Box>
-
-                <FormControl>
-                  <FormLabel>Descripción del Curso</FormLabel>
-                  <Textarea
-                    type="text"
-                    placeholder="Escriba una descripción del curso..."
-                    id="description"
-                    name="description"
-                    rows={5}
-                    {...formik.getFieldProps("description")}
-                  />
+                        <Text
+                          position="absolute"
+                          top="50%"
+                          left="50%"
+                          transform="translate(-50%, -50%)"
+                          color="white"
+                          textShadow="0 0 10px black"
+                          fontWeight={600}
+                        >
+                          Drag and drop your image here
+                        </Text>
+                        <input
+                          id="thumbnail"
+                          name="thumbnail"
+                          style={{ display: "none" }}
+                          type="file"
+                          accept=".jpg,.jpeg,.png"
+                          onChange={handleImageChange}
+                        />
+                        <IconButton
+                          icon={<BsPencilSquare />}
+                          aria-label="Change Image"
+                          rounded="full"
+                          variant="outlined"
+                          display={isDraggingOver ? "none" : "inherit"}
+                          onClick={() =>
+                            document.getElementById("thumbnail").click()
+                          }
+                        />
+                      </Box>
+                    </Box>
+                  </AspectRatio>
+                  <FormHelperText>
+                    Imagen del curso (Aspect Ratio 16/9)
+                  </FormHelperText>
+                  <FormErrorMessage>{formik.errors.thumbnail}</FormErrorMessage>
                 </FormControl>
 
-                <Box>
+                <VStack w="full">
                   <FormControl>
-                    <FormLabel>Etiquetas del curso</FormLabel>
-                    <ChakraTagInput
-                      rounded={".25em"}
-                      border={"1px"}
-                      borderColor="gray.300"
-                      _hover={{
-                        borderColor: "black",
-                        boxShadow: "0 0 0 1px blue.300",
-                      }}
-                      _focusWithin={{
-                        outline: "2px solid",
-                        outlineColor: "blue.100",
-                        outlineOffset: "0px",
-                      }}
-                      id="keywords"
-                      name="keywords"
-                      tags={tags}
-                      onTagsChange={handleTagsChange}
+                    <FormLabel>Nombre del Curso</FormLabel>
+                    <Input
+                      id="name"
+                      name="name"
+                      {...formik.getFieldProps("name")}
                     />
+                    <FormErrorMessage>{formik.errors.name}</FormErrorMessage>
+                  </FormControl>
+
+                  <FormControl>
+                    <FormLabel>Slug del Curso</FormLabel>
+                    <HStack>
+                      <Input
+                        id="slug"
+                        name="slug"
+                        {...formik.getFieldProps("slug")}
+                      />
+                      <Button
+                        onClick={() => generateSlug(formik.values.slug)}
+                        variant="primary"
+                      >
+                        Generar
+                      </Button>
+                    </HStack>
+                    <FormErrorMessage>{formik.errors.slug}</FormErrorMessage>
+                  </FormControl>
+
+                  <FormControl>
+                    <FormLabel>Categoría</FormLabel>
+                    <Select
+                      placeholder="Selecciona una categoría"
+                      id="category"
+                      name="category"
+                      {...formik.getFieldProps("category")}
+                    >
+                      {categories?.map((category) => (
+                        <option key={category.id} value={category.id}>
+                          {category.name}
+                        </option>
+                      ))}
+                    </Select>
                     <FormErrorMessage>
-                      {formik.errors.keywords}
+                      {formik.errors.category}
                     </FormErrorMessage>
                   </FormControl>
-                </Box>
-                <Stack spacing={10} pt={2} p={1}>
-                  <Button leftIcon={<BsSave />} variant="primary" type="submit">
-                    Guardar cambios
-                  </Button>
-                </Stack>
+                </VStack>
+              </Box>
+
+              <FormControl>
+                <FormLabel>Descripción del Curso</FormLabel>
+                <Textarea
+                  type="text"
+                  placeholder="Escriba una descripción del curso..."
+                  id="description"
+                  name="description"
+                  rows={5}
+                  {...formik.getFieldProps("description")}
+                />
+                <FormErrorMessage>
+                  {formik.touched.description && formik.errors.description}
+                </FormErrorMessage>
+              </FormControl>
+
+              <Box>
+                <FormControl>
+                  <FormLabel>Etiquetas del curso</FormLabel>
+                  <ChakraTagInput
+                    rounded={".25em"}
+                    border={"1px"}
+                    borderColor="gray.300"
+                    _hover={{
+                      borderColor: "black",
+                      boxShadow: "0 0 0 1px blue.300",
+                    }}
+                    _focusWithin={{
+                      outline: "2px solid",
+                      outlineColor: "blue.100",
+                      outlineOffset: "0px",
+                    }}
+                    id="keywords"
+                    name="keywords"
+                    tags={tags}
+                    onTagsChange={handleTagsChange}
+                  />
+                  <FormErrorMessage>
+                    {formik.touched.keywords && formik.errors.keywords}
+                  </FormErrorMessage>
+                </FormControl>
+              </Box>
+              <Stack spacing={10} pt={2} p={1}>
+                <Button leftIcon={<BsSave />} variant="primary" type="submit">
+                  Guardar cambios
+                </Button>
               </Stack>
-            </form>
-          </Box>
-        </HStack>
+            </Stack>
+          </form>
+        </Box>
       </Layout>
     </>
   );
